ec7b63db6cf7cd78c1f5f29234ce78fe6a2e2305
KravchukIren <60760596+KravchukIren@users.noreply.github.com>
2020-03-14 05:50:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function changeLamp() {
+  const button = document.getElementById("turnOn");
+  const pTag = document.getElementById("changeText");
+  const body = document.getElementById("changeColor");
+  const lamp = document.getElementsByClassName("fas fa-lightbulb");
+  pTag.style.color = "black";
+  pTag.innerHTML = "Bulb is: ON";
+  body.style.backgroundColor = "white";
+  lamp.style.color = "yellow";
+}
+
+function changeBack() {
+  const button = document.getElementById("turnOn");
+  const pTag = document.getElementById("changeText");
+  const body = document.getElementById("changeColor");
+  const lamp = document.getElementsByClassName("fas fa-lightbulb");
+  pTag.style.color = "beige";
+  pTag.innerHTML = "Bulb is: OFF";
+  body.style.backgroundColor = "grey";
+  lamp.style.color = "black";
+}
