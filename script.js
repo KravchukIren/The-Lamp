e5d16bc024d6f7400908ed4531fc01fd3fb7484e
KravchukIren <60760596+KravchukIren@users.noreply.github.com>
2020-03-17 01:36:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function changeLamp() {
   const button = document.getElementById("turnOn");
   const pTag = document.getElementById("changeText");
   const body = document.getElementById("changeColor");
-  const lamp = document.getElementsByClassName("fas fa-lightbulb");
+  const lamp = document.getElementsByClassName("fas fa-lightbulb")[0];
   pTag.style.color = "black";
   pTag.innerHTML = "Bulb is: ON";
   body.style.backgroundColor = "white";
@@ -13,7 +13,7 @@ function changeBack() {
   const button = document.getElementById("turnOn");
   const pTag = document.getElementById("changeText");
   const body = document.getElementById("changeColor");
-  const lamp = document.getElementsByClassName("fas fa-lightbulb");
+  const lamp = document.getElementsByClassName("fas fa-lightbulb")[0];
   pTag.style.color = "beige";
   pTag.innerHTML = "Bulb is: OFF";
   body.style.backgroundColor = "grey";
